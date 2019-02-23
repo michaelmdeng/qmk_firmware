d54f13baabcf1989084b2ae31e762eaee42cd960
Michael Deng <michaelmdeng@gmail.com>
2019-02-22 22:54:51 -0800

    add my custom keymap

---
+++ keyboards/keebio/nyquist/keymaps/miden/keymap.c
@@ -0,0 +1,171 @@
+#include QMK_KEYBOARD_H
+
+extern keymap_config_t keymap_config;
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _QWERTY 0
+#define _LOWER 3
+#define _RAISE 4
+#define _ADJUST 16
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  ADJUST,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+
+/* Qwerty
+ * |----------|-----|-----|-------|-------|-------|-------|-------|-------|-------|------|--------|
+ * | Esc      | 1   | 2   | 3     | 4     | 5     | 6     | 7     | 8     | 9     | 0    | Bksp   |
+ * |----------|-----|-----|-------|-------|-------|-------|-------|-------|-------|------|--------|
+ * | Tab      | Q   | W   | E     | R     | T     | Y     | U     | I     | O     | P    | NONE   |
+ * |----------|-----|-----|-------|-------|-------|-------|-------|-------|-------|------|--------|
+ * | LCtl/Esc | A   | S   | D     | F     | G     | H     | J     | K     | L     | ;    | Enter  |
+ * |----------|-----|-----|-------|-------|-------|-------|-------|-------|-------|------|--------|
+ * | LShift   | Z   | X   | C     | V     | B     | N     | M     | ,     | .     | /    | RShift |
+ * |----------|-----|-----|-------|-------|-------|-------|-------|-------|-------|------|--------|
+ * | LCtl     | GUI | Alt | Raise | Lower | Space | Space | Space | Lower | Raise | Menu | RCtl   |
+ * |----------|-----|-----|-------|-------|-------|-------|-------|-------|-------|------|--------|
+ */
+[_QWERTY] = LAYOUT( \
+  KC_ESC,        KC_1,    KC_2,    KC_3,  KC_4,  KC_5,   KC_6,   KC_7,   KC_8,    KC_9,   KC_0,    KC_BSPC, \
+  KC_TAB,        KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,    XXXXXXX, \
+  CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,  KC_F,  KC_G,   KC_H,   KC_J,   KC_K,    KC_L,   KC_SCLN, KC_ENT,  \
+  KC_LSFT,       KC_Z,    KC_X,    KC_C,  KC_V,  KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
+  KC_LCTL,       KC_LGUI, KC_LALT, RAISE, LOWER, KC_SPC, KC_SPC, KC_SPC, LOWER,   RAISE,  KC_MENU, KC_RCTL  \
+),
+
+
+/* Lower
+ *
+ * Extended symbols, Vim navigation, and common hotkeys
+ *
+ * |---|------|-----|------|-----------|--|-------|------|--------|-------|---|---|
+ * | ` |      |     |      |           |  |       |      |        |       | - | = |
+ * |---|------|-----|------|-----------|--|-------|------|--------|-------|---|---|
+ * |   |      |     | Wh D |           |  | Wh Up |      |        |       | [ | ] |
+ * |---|------|-----|------|-----------|--|-------|------|--------|-------|---|---|
+ * |   |      |     |      | Find      |  | Left  | Down | Up     | Right |   |   |
+ * |---|------|-----|------|-----------|--|-------|------|--------|-------|---|---|
+ * |   | Undo | Cut | Copy | Paste     |  |       |      |        |       | \ | ' |
+ * |---|------|-----|------|-----------|--|-------|------|--------|-------|---|---|
+ * |   |      |     |      | Qwerty(T) |  |       |      | Qwerty |       |   |   |
+ * |---|------|-----|------|-----------|--|-------|------|--------|-------|---|---|
+ */
+
+[_LOWER] = LAYOUT( \
+  KC_GRV,  _______, _______, _______, _______,     _______, _______, _______, _______, _______,  KC_MINUS, KC_EQL,  \
+  _______, _______, _______, KC_WH_D, _______,     _______, KC_WH_U, _______, _______, _______,  KC_LBRC,  KC_RBRC, \
+  _______, _______, _______, _______, KC_FIND,     _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______,  _______, \
+  _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE,    _______, _______, _______, _______, _______,  KC_BSLS,  KC_QUOT, \
+  _______, _______, _______, _______, OSL(QWERTY), _______, _______, _______, QWERTY,  _______,  _______,  _______ \
+),
+
+/* Raise
+ *
+ * Extended commands, Page navigation, and function keys
+ *
+ * |------|-----|-----|-----------|----|----|------|-------|------|--------|-----|-----|
+ * |      | F1  | F2  | F3        | F4 | F5 | F6   | F7    | F8   | F9     | F10 | Del |
+ * |------|-----|-----|-----------|----|----|------|-------|------|--------|-----|-----|
+ * |      | F11 | F12 |           |    |    |      |       |      |        |     |     |
+ * |------|-----|-----|-----------|----|----|------|-------|------|--------|-----|-----|
+ * | Caps |     |     |           |    |    | Home | PgDwn | PgUp | End    |     |     |
+ * |------|-----|-----|-----------|----|----|------|-------|------|--------|-----|-----|
+ * |      |     |     |           |    |    |      |       |      |        |     |     |
+ * |------|-----|-----|-----------|----|----|------|-------|------|--------|-----|-----|
+ * |      |     |     | Qwerty(T) |    |    |      |       |      | Qwerty |     |     |
+ * |------|-----|-----|-----------|----|----|------|-------|------|--------|-----|-----|
+ */
+[_RAISE] = LAYOUT( \
+  KC_GRV,  KC_F1,   KC_F2,   KC_F3,       KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,  \
+  _______, KC_F11,  KC_F12,  _______,     _______, _______, _______, _______, _______, _______, _______, _______, \
+  KC_CAPS, _______, _______, _______,     _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______, \
+  _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, OSL(QWERTY), _______, _______, _______, _______, _______, QWERTY,  _______, _______  \
+),
+
+/* Adjust (Lower + Raise)
+ * ,-----------------------------------------------------------------------------------.
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      | Reset|RGB Tg|RGB Md|Hue Up|Hue Dn|Sat Up|Sat Dn|Val Up|Val Dn|      |  Del |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_ADJUST] =  LAYOUT( \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,  \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+)
+
+
+};
+
+#ifdef AUDIO_ENABLE
+float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
+float tone_dvorak[][2]     = SONG(DVORAK_SOUND);
+float tone_colemak[][2]    = SONG(COLEMAK_SOUND);
+#endif
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        #ifdef AUDIO_ENABLE
+          PLAY_SONG(tone_qwerty);
+        #endif
+        persistent_default_layer_set(1UL<<_QWERTY);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        /* update_tri_layer(_LOWER, _RAISE, _ADJUST); */
+      } else {
+        layer_off(_LOWER);
+        /* update_tri_layer(_LOWER, _RAISE, _ADJUST); */
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        /* update_tri_layer(_LOWER, _RAISE, _ADJUST); */
+      } else {
+        layer_off(_RAISE);
+        /* update_tri_layer(_LOWER, _RAISE, _ADJUST); */
+      }
+      return false;
+      break;
+    case ADJUST:
+      if (record->event.pressed) {
+        layer_on(_ADJUST);
+      } else {
+        layer_off(_ADJUST);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
